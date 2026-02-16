50dcd6fb1822bc21886f1bcf88e3778141bfb92f
s002bxs-75 <s002bxs@gmail.com>
2026-02-16 08:05:07 -0500

    Update install-utilities.sh

--- install-utilities.sh
+++ install-utilities.sh
@@ -3,4 +3,6 @@
 echo "http://plug-mirror.rcac.purdue.edu/alpine/edge/main" | doas tee -a "/etc/apk/repositories"
 echo "http://plug-mirror.rcac.purdue.edu/alpine/edge/community" | doas tee -a "/etc/apk/repositories"
 echo "http://plug-mirror.rcac.purdue.edu/alpine/edge/testing" | doas tee -a "/etc/apk/repositories"
+doas apk update
+doas apk upgrade
 doas apk add dislocker fuse3 ntfs-3g qemu-img vlc vlc-qt
