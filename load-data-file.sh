e5cc13fb8fafc531550a1ad451175063f00b9c59
Benjamin Sebastian <sbenjamin@AlpineLinuxBrowser.attlocal.net>
2026-03-22 18:32:47 -0400

    Updated utilities.

--- load-data-file.sh
+++ load-data-file.sh
@@ -14,5 +14,9 @@ else
     doas qemu-nbd -c /dev/nbd0 "$1"
     doas fdisk -l /dev/nbd0
     doas dislocker -V /dev/nbd0p1 -u"$2" -- ~/Desktop/BitlockerData
-    doas mount -o loop -t ntfs ~/Desktop/BitlockerData/dislocker-file ~/Desktop/Data
+    #doas mount -o loop -t fat ~/Desktop/BitlockerData/dislocker-file ~/Desktop
+    #doas mount -o loop -t vfat ~/Desktop/BitlockerData/dislocker-file ~/Desktop
+    #doas mount -o loop -t exfat ~/Desktop/BitlockerData/dislocker-file ~/Desktop
+    #doas mount -o loop -t ext4 ~/Desktop/BitlockerData/dislocker-file ~/Desktop
+    #doas mount -o loop -t ntfs ~/Desktop/BitlockerData/dislocker-file ~/Desktop/Data
 fi
--- unload.sh
+++ unload.sh
@@ -3,6 +3,10 @@
 doas umount ~/Desktop/Data
 doas umount ~/Desktop/BitlockerData
 rm -rf ~/Desktop/Data ~/Desktop/BitlockerData
+
+doas umount ~/Desktop/VBoxShare
+rm -rf ~/Desktop/VBoxShare
+
 doas qemu-nbd -d /dev/nbd0
 doas umount ~/Desktop/Drive
 doas umount ~/Desktop/BitlockerDrive
